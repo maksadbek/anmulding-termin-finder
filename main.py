d31624e58e61270b7d4e9cfca46308ad0bf1816a
Maksadbek Akhmedov <maksadbek.akhmedov@hellofresh.com>
2023-04-10 21:22:43 +0200

    reformat

--- main.py
+++ main.py
@@ -13,71 +13,67 @@ import telegram
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-
 bot_api_key = os.getenv("BOT_API_KEY")
 chat_id = os.getenv("CHAT_ID")
 
 bot = telegram.Bot(bot_api_key)
-
 send_message = partial(bot.send_message, chat_id=chat_id)
 
 
 def get_url_and_cookies():
-	r = requests.get("https://service.berlin.de/dienstleistung/120686/")
-	r.raise_for_status()
+    r = requests.get("https://service.berlin.de/dienstleistung/120686/")
+    r.raise_for_status()
 
-	if r.status_code == 200:
-		soup = BeautifulSoup(r.text, features="html.parser")
-		for link in soup.find_all("a"):
-			if link.text == "Termin berlinweit suchen":
-				return link.get("href"), r.cookies
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text, features="html.parser")
+        for link in soup.find_all("a"):
+            if link.text == "Termin berlinweit suchen":
+                return link.get("href"), r.cookies
 
-	raise Exception(f"unexpected error {r.status_code}")
+    raise Exception(f"unexpected error {r.status_code}")
 
 
 class RefreshCookieException(Exception):
-	pass
+    pass
 
 
 async def check_booking_availability(url, cookies):
-	# (url, cookies) = get_url_and_cookies()
+    r = requests.get(url, cookies=cookies)
+    r.raise_for_status()
 
-	# print(url, cookies)
+    soup = BeautifulSoup(r.text, features="html.parser")
+    # print(soup.prettify())
 
-	r = requests.get(url, cookies=cookies)
-	r.raise_for_status()
+    for buchbar in soup.find_all("td.buchbar"):
+        send_message(text=buchbar.__str__)
+        print("buchbar:", buchbar)
 
-	soup = BeautifulSoup(r.text, features="html.parser")
-	# print(soup.prettify())
+    nicht_count = 0
+    for nichtbuchbar in soup.select("td.nichtbuchbar"):
+        nicht_count += 1
 
-	for buchbar in soup.find_all("td.buchbar"):
-		send_message(text=buchbar.__str__)
-		print("buchbar:", buchbar)
+    if nicht_count == 0:
+        raise RefreshCookieException
 
-	nicht_count = 0
-	for nichtbuchbar in soup.select("td.nichtbuchbar"):
-		nicht_count += 1
+    print(nicht_count)
 
-	if nicht_count == 0:
-		raise RefreshCookieException
+    if random.randint(0, 100) == 50:
+        print(soup.prettify())
 
-	if random.randint(0, 100) == 50:
-		print(soup.prettify())
-
-	print(datetime.now())
+    print(datetime.now())
 
 
 async def main():
-	(url, cookies) = get_url_and_cookies()
+    (url, cookies) = get_url_and_cookies()
 
-	while True:
-		await check_booking_availability(url, cookies)
-		time.sleep(30)
+    while True:
+        await check_booking_availability(url, cookies)
+        time.sleep(30)
 
 
 if __name__ == "__main__":
-	while True:
-		try:
-			asyncio.run(main())
-		except RefreshCookieException:
-			print("refresh")
+    while True:
+        try:
+            asyncio.run(main())
+        except RefreshCookieException:
+            print("refresh")
